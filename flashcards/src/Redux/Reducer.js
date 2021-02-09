ae63f48326a57fd6565b0824d293291ab8c0b148
Joe-Thompson <bmo3696@gmail.com>
2021-02-08 20:11:39 -0500

    have working choices

--- flashcards/src/Redux/Reducer.js
+++ flashcards/src/Redux/Reducer.js
@@ -2,7 +2,12 @@ import { SORT_CHAPTERS } from "./Actions";
 import {act} from "@testing-library/react";
 
 const initialState = {
-    card: []
+    card: [],
+    one: null,
+    two: null,
+    three: null,
+    four: null,
+    five: null
 };
 
 export default function mainReducer( state = initialState, action) {
@@ -10,7 +15,11 @@ export default function mainReducer( state = initialState, action) {
         case SORT_CHAPTERS:
             return {
                 ...state,
-                card: action.payload.data
+                one: action.payload.chapterOne,
+                two: action.payload.chapterTwo,
+                three: action.payload.chapterThree,
+                four: action.payload.chapterFour,
+                five: action.payload.chapterFive
             }
     }
 }
