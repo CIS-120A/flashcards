ed1819d733ec21dc899a8a939654028965f00679
Joe-Thompson <bmo3696@gmail.com>
2021-04-28 18:55:44 -0400

    added selects for new chapters

--- flashcards/src/components/FlashCards.js
+++ flashcards/src/components/FlashCards.js
@@ -10,30 +10,19 @@ const FlashCard_Study = ({ data, history, match }) => {
     const [counter, setCounter] = useState(0);
     const id = match.params.id;
 
-    const clickHandler = (e) => {
-        e.preventDefault();
-        setFlipped(false)
-        setCounter(counter + 1);
-        if (counter === flashCards.length - 1) {
-            setCounter(0)
-        }
-    };
-
     const click_handler = (e) => {
         e.preventDefault()
         if (e.target.textContent === "Random") {
-            if (flipped) {
+
+            if (flipped === true) {
                 setFlipped(!flipped)
-                e.target.className = "capitalize"
                 setCounter(Math.floor(Math.random() * Math.floor(flashCards.length)));
             } else {
-                e.target.className = "capitalize"
                 setCounter(Math.floor(Math.random() * Math.floor(flashCards.length)));
             }
         } else if (e.target.textContent === "Next") {
-            if (flipped) {
+            if (flipped === true) {
                 setFlipped(!flipped)
-                e.target.className = "capitalize"
 
                 setCounter(counter + 1);
                 if (counter === flashCards.length - 1) {
@@ -46,37 +35,18 @@ const FlashCard_Study = ({ data, history, match }) => {
                 }
             }
         } else {
-            if (!flipped) {
-                setFlipped(!flipped)
-                e.target.className = ""
-            } else {
-                setFlipped(!flipped)
-                e.target.className = "capitalize"
-            }
+            click_flip(e);
         }
     }
 
     const click_flip = (e) => {
         e.preventDefault();
-
-        if (e.target.className === "capitalize") {
-            e.target.className = "";
+        console.log("click flip")
+        if (flipped === true) {
+            setFlipped(false);
         } else {
-            e.target.className = "capitalize";
+            setFlipped(true)
         }
-        setFlipped(!flipped);
-    };
-
-    const click_random = (e) => {
-      e.preventDefault();
-      console.log(e.target)
-      if (e.target.className === "capitalize") {
-            e.target.className = "";
-        } else {
-            e.target.className = "capitalize";
-        }
-      setFlipped(false)
-      setCounter(Math.floor(Math.random() * Math.floor(flashCards.length)));
     };
 
     const click_home = (e) => {
@@ -95,7 +65,7 @@ const FlashCard_Study = ({ data, history, match }) => {
                     <button onClick={click_handler}>Next</button>
                     <button onClick={click_handler}>Random</button>
                     </div>
-                <h1 className="capitalize" onClick={click_handler}>{flipped ? flashCards[counter].definition : flashCards[counter].term}</h1>
+                <h1 onClick={click_handler}>{flipped ? flashCards[counter].definition : flashCards[counter].term}</h1>
                 </div>
 
                 <div className='fc_instructions'>
